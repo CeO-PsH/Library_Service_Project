b2676c93143afa55c481b8f408603c7db0f549d8
Nazar Psheiuk <nazar.psheiuk@gmail.com>
2023-09-11 19:09:47 +0300

    Created book model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+from Library_Service_Project import settings
+
+
+class BookModel(models.Model):
+    class Enum(models.TextChoices):
+        HARD = "HARD"
+        SOFT = "SOFT"
+
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(
+        max_length=5,
+        choices=Enum.choices,
+        default=Enum.HARD
+    )
+    inventory = models.IntegerField(unique=True)
+    daily_fee = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.title}, Author: {self.author}, Inventory: {self.inventory}"
