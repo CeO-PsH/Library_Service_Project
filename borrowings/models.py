85137e564eef791386081b28c7645fe2e4fcff08
Nazar Psheiuk <nazar.psheiuk@gmail.com>
2023-09-12 15:34:42 +0300

    changes blank and nul in a borrowing actual field model

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from rest_framework.exceptions import ValidationError
 
 from Library_Service_Project import settings
 from books.models import Books
@@ -6,7 +7,7 @@ from books.models import Books
 class Borrowing(models.Model):
     borrow_date = models.DateTimeField(auto_now=True)
     expected_return_date = models.DateTimeField(auto_now=False)
-    actual_return_date = models.DateTimeField(auto_now=True)
+    actual_return_date = models.DateTimeField(auto_now=False, blank=True, null=True)
     book = models.ForeignKey(Books, related_name="borrowings", on_delete=models.CASCADE)
     user =  models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE
@@ -16,6 +17,37 @@ class Borrowing(models.Model):
         models.UniqueConstraint(
             fields=["borrow_date", "expected_return_date", "actual_return_date"],
             name="unique date"
+        )
+
+    @staticmethod
+    def validate_inventory(inventory: int, error_to_raise):
+        for inventory_attr_value, inventory_attr_name in [
+            (inventory, "numbers"),
+        ]:
+            if inventory_attr_value <= 0:
+                raise error_to_raise(
+                    {
+                        inventory_attr_name:
+                            f" The {inventory_attr_name} of inventory items must be greater than 0."
+                    }
+                )
+
+    def clean(self):
+        Borrowing.validate_inventory(
+            self.book.inventory,
+            ValidationError,
+        )
+
+    def save(
+        self,
+        force_insert=False,
+        force_update=False,
+        using=None,
+        update_fields=None,
+    ):
+        self.full_clean()
+        return super(Borrowing, self).save(
+            force_insert, force_update, using, update_fields
         )
 
     def __str__(self):
