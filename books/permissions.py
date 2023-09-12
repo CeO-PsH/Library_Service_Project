8461a40e71b298058aa76b75e1216bd869473f33
Nazar Psheiuk <nazar.psheiuk@gmail.com>
2023-09-12 12:49:41 +0300

    Change permissions.py added permissions in view

--- books/permissions.py
+++ books/permissions.py
@@ -4,10 +4,6 @@ from rest_framework.permissions import SAFE_METHODS, BasePermission
 class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
         return bool(
-            (
-                request.method in SAFE_METHODS or
-                request.user and
-                request.user.is_authenticated
+                request.method in SAFE_METHODS or request.user.is_staff
             )
-            or (request.user and request.user.is_staff)
-        )
+
--- books/views.py
+++ books/views.py
@@ -3,10 +3,11 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
 from books.models import Books
+from books.permissions import IsAdminOrReadOnly
 from books.serializers import BooksSerializer
 
 
 class BooksViewSet(viewsets.ModelViewSet):
     queryset = Books.objects.all()
     serializer_class = BooksSerializer
-    permission_classes = (IsAuthenticated, )
+    permission_classes = (IsAdminOrReadOnly, )
