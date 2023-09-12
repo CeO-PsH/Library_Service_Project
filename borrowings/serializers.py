5a4bd9b0d925a18e30a5236a3b5f04739660a27f
Nazar Psheiuk <nazar.psheiuk@gmail.com>
2023-09-12 15:38:39 +0300

    added BorrowingSerializer, validation, BorrowingListSerializer, BorrowingCreateSerializer, BorrowingDetailSerializer, change view and added perform_create

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -1,12 +1,37 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from borrowings.models import Borrowing
 from books.serializers import BooksSerializer
 
-class BorrowingListSerializer(serializers.ModelSerializer):
-    book = BooksSerializer(many=False, read_only=True)
+class BorrowingSerializer(serializers.ModelSerializer):
+
+    def validate(self, attrs):
+        data = super(BorrowingSerializer, self).validate(attrs=attrs)
+        Borrowing.validate_inventory(
+            attrs["book"].inventory,
+            ValidationError
+        )
+        return data
 
     class Meta:
         model = Borrowing
-        fields = ("id", "borrow_date", "expected_return_date", "actual_return_date", "book", "user" )
+        fields = ("id", "borrow_date", "expected_return_date", "actual_return_date", "book",)
 
+class BorrowingListSerializer(BorrowingSerializer):
+    book = serializers.StringRelatedField(many=False)
+
+    class Meta:
+        model = Borrowing
+        fields = ("id", "borrow_date", "expected_return_date", "actual_return_date", "book")
+
+class BorrowingCreateSerializer(BorrowingSerializer):
+    class Meta:
+        model = Borrowing
+        fields = ("borrow_date", "expected_return_date", "book",)
+
+class BorrowingDetailSerializer(BorrowingSerializer):
+    book = BooksSerializer(many=False, read_only=True)
+    class Meta:
+        model = Borrowing
+        fields = ("id", "borrow_date", "expected_return_date", "actual_return_date", "book", "user" ,)
--- borrowings/views.py
+++ borrowings/views.py
@@ -1,11 +1,35 @@
-from django.shortcuts import render
 from rest_framework import mixins
 from rest_framework.viewsets import GenericViewSet
 
+from books.models import Books
 from borrowings.models import Borrowing
-from borrowings.serializers import BorrowingListSerializer
+from borrowings.serializers import (
+    BorrowingListSerializer,
+    BorrowingCreateSerializer,
+    BorrowingSerializer,
+    BorrowingDetailSerializer,
+)
 
 
-class BorrowingListViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin,GenericViewSet):
+class BorrowingListViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.CreateModelMixin, GenericViewSet):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingListSerializer
+
+
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BorrowingListSerializer
+        if self.action == "retrieve":
+            return BorrowingDetailSerializer
+        if self.action == "create":
+            return BorrowingCreateSerializer
+
+        return BorrowingSerializer
+
+    def perform_create(self, serializer):
+        book_get = self.request.data.get("book", None)
+        book = Books.objects.get(id=book_get)
+        book.inventory -= 1
+        book.save()
+        serializer.save(user=self.request.user)
