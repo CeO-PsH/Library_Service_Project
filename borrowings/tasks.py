57358afab531f8ad1443e8a9a0d42d5e130ca4e2
Nazar Psheiuk <nazar.psheiuk@gmail.com>
2023-09-13 17:01:38 +0300

    create a new task

---
+++ borrowings/tasks.py
@@ -0,0 +1,18 @@
+from borrowings.models import Borrowing
+
+from celery import shared_task
+from datetime import date, timedelta
+from borrowings.send_messege_to_telegram import send_to_telegram
+
+@shared_task
+def send_message_about_borrowing_books() -> None:
+    tomorrow = date.today() + timedelta(days=2)
+    borrowing_book = Borrowing.objects.filter(
+        is_active=True,
+        expected_return_date__lt=tomorrow
+    )
+    if len(borrowing_book) > 0:
+        for book in borrowing_book:
+            send_to_telegram(f"Title: {book.book.title}, expected return date: {book.expected_return_date}, Customer{book.user.id}")
+    else:
+        send_to_telegram("No borrowings overdue today!")
