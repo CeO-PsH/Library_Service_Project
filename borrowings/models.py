1c469c2daa6225d03c31440203ff8e9920fccb3a
Nazar Psheiuk <nazar.psheiuk@gmail.com>
2023-09-14 15:49:59 +0300

    added model payments in models.py

--- borrowings/models.py
+++ borrowings/models.py
@@ -60,7 +60,7 @@ class Payment(models.Model):
         PAID = "PAID"
 
     class Type(models.TextChoices):
-        PAYMENT   = "PAYMENT  "
+        PAYMENT   = "PAYMENT"
         FINE = "FINE"
     status = models.CharField(max_length=25, choices=Status.choices, default=Status.PENDING)
     type =  models.CharField(max_length=25, choices=Type.choices, default=Type.PAYMENT)
@@ -68,3 +68,6 @@ class Payment(models.Model):
     session_url = models.URLField()
     session_id = models.CharField(max_length=255)
     money_to_pay = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.id}.Status: {self.status}, Type:{self.type}, Money to pay: {self.money_to_pay} USD"
