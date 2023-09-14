86655cd9a92be7a502218cdba5050dfffbae2869
Nazar Psheiuk <nazar.psheiuk@gmail.com>
2023-09-14 19:20:21 +0300

    create urls success and canceled, change api/borrowings in library urls

--- borrowings/urls.py
+++ borrowings/urls.py
@@ -6,16 +6,19 @@ from borrowings.views import (
     BorrowingListViewSet,
     return_borrowing,
     PaymentsViewSet,
-    #create_checkout_session,
+    order_success,
+    order_canceled,
+    # create_checkout_session,
 )
 
 app_name = "borrowing"
 router = routers.DefaultRouter()
-router.register("borrowing", BorrowingListViewSet)
+router.register("borrowings", BorrowingListViewSet)
 router.register("payments", PaymentsViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
     path("<int:pk>/return/", return_borrowing, name="return"),
-
+    path("success", order_success),
+    path("canceled/", order_canceled),
 ]
