ebec2d257e7f7f361c4285cd2e7b8a59af559f17
Nazar Psheiuk <nazar.psheiuk@gmail.com>
2023-09-14 19:22:08 +0300

    added view order success with func of change in payment instance. create a canceled view set

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,10 +1,12 @@
 import os
 from datetime import datetime
 
+import requests
 import stripe
 from django.db import transaction
-from django.http import JsonResponse, HttpResponseRedirect
-from django.views.decorators.csrf import csrf_exempt
+from django.http import request, HttpResponse
+from django.http.request import HttpRequest
+from django.template.loader import render_to_string
 from rest_framework import mixins, status
 from rest_framework.decorators import api_view
 from django.shortcuts import get_object_or_404, redirect
@@ -13,7 +15,6 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
-from Library_Service_Project import settings
 from books.models import Books
 from borrowings.models import Borrowing, Payment
 from borrowings.serializers import (
@@ -25,11 +26,17 @@ from borrowings.serializers import (
     PaymentsDetailSerializer,
     PaymentsSerializer,
 )
-from .send_messege_to_telegram import  send_to_telegram
+from .send_messege_to_telegram import send_to_telegram
 
-class BorrowingListViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.CreateModelMixin, GenericViewSet):
+
+class BorrowingListViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin,
+    GenericViewSet,
+):
     queryset = Borrowing.objects.all()
-    permission_classes = (IsAuthenticated, )
+    permission_classes = (IsAuthenticated,)
 
     @staticmethod
     def _params_to_ints(qs):
@@ -59,21 +66,23 @@ class BorrowingListViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, mix
         if self.action == "create":
             return BorrowingCreateSerializer
 
-
         return BorrowingSerializer
 
     def perform_create(self, serializer):
         with transaction.atomic():
             data = self.request.data
-            book = Books.objects.get(id=int(data['book']))
+            book = Books.objects.get(id=int(data["book"]))
             book.inventory -= 1
             book.save()
             serializer.save(user=self.request.user)
-            create_checkout_session(serializer.data['id'])
-            send_to_telegram(f"Borrowing №: {serializer.data['id']} Title: {book.title} Borrowing at:{datetime.now()}. Expected return date: {data['expected_return_date']}")
+            create_checkout_session(serializer.data["id"])
+            send_to_telegram(
+                f"Borrowing №: {serializer.data['id']} Title: {book.title} Borrowing at:{datetime.now()}. Expected return date: {data['expected_return_date']}"
+            )
+
 
 @api_view(["POST", "GET"])
-def  return_borrowing(request: Request, pk) -> Response:
+def return_borrowing(request: Request, pk) -> Response:
     with transaction.atomic():
         borrowing = get_object_or_404(Borrowing, id=pk)
         if borrowing.is_active:
@@ -82,17 +91,28 @@ def  return_borrowing(request: Request, pk) -> Response:
             borrowing.is_active = False
             borrowing.save()
             serializer = BorrowingDetailSerializer(borrowing)
-            send_to_telegram(f"Borrowing №: {borrowing.id}, Title: {borrowing.book} was returned at: {borrowing.actual_return_date}")
+            send_to_telegram(
+                f"Borrowing №: {borrowing.id}, Title: {borrowing.book} was returned at: {borrowing.actual_return_date}"
+            )
             return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response({"detail": "The book has already been returned."}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(
+            {"detail": "The book has already been returned."},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
 
-class PaymentsViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.CreateModelMixin, GenericViewSet):
+
+class PaymentsViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin,
+    GenericViewSet,
+):
     queryset = Payment.objects.all()
 
     def get_queryset(self):
         queryset = self.queryset
         if self.request.user.is_staff is False:
-             queryset = self.queryset.filter(borrowing__user=self.request.user)
+            queryset = self.queryset.filter(borrowing__user=self.request.user)
         return queryset
 
     def get_serializer_class(self):
@@ -123,25 +143,39 @@ def create_checkout_session(pk):
             line_items=[
                 {
                     "price_data": {
-                     "currency": "usd",
+                        "currency": "usd",
                         "unit_amount_decimal": price,
-                        'product_data': {
-                            'name': borrowing.book.title,
-                            'description': f"Author: {borrowing.book.author} "
+                        "product_data": {
+                            "name": borrowing.book.title,
+                            "description": f"Author: {borrowing.book.author} ",
                         },
                     },
                     "quantity": 1,
                 }
             ],
-             mode='payment',
-            success_url=LOCAL_DOMAIN + 'borrowings/success/',
-            cancel_url=LOCAL_DOMAIN + 'borrowings/cancel/',
+            mode="payment",
+            success_url=LOCAL_DOMAIN
+            + "api/borrowings/success?session_id={CHECKOUT_SESSION_ID}",
+            cancel_url=LOCAL_DOMAIN + "api/borrowings/canceled/",
         )
         Payment.objects.create(
             borrowing=borrowing,
             session_url=checkout_session.url,
             session_id=checkout_session.stripe_id,
-            money_to_pay=checkout_session.amount_total
+            money_to_pay=checkout_session.amount_total,
         )
     except Exception as e:
         return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+@api_view(["GET"])
+def order_success(request):
+    session = stripe.checkout.Session.retrieve(request.query_params["session_id"])
+    payment = Payment.objects.get(session_id=session.stripe_id)
+    payment.status = "PAID"
+    payment.save()
+    return redirect("http://127.0.0.1:8000/api/borrowings/borrowing/")
+
+@api_view(["GET"])
+def order_canceled(request):
+    cancel_message = "Payment can be paid a bit later, but the session is available for only 24 hours."
+    return HttpResponse(cancel_message)
